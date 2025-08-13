9e2c172cd146b5fe63ba7f3454a9e240dbd6fd50
mokshsinghji <moksh@choudry.org>
2025-08-13 21:55:39 +0100

    created react frontend with tanstack start spa mode, and set up api proxy :)

---
+++ react-frontend/vite.config.ts
@@ -0,0 +1,34 @@
+import {defineConfig} from 'vite'
+import {tanstackStart} from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+    plugins: [
+        // this is the plugin that enables path aliases
+        viteTsConfigPaths({
+            projects: ['./tsconfig.json'],
+        }),
+        tailwindcss(),
+        tanstackStart({
+            spa: {
+                enabled: true
+            },
+            customViteReactPlugin: true,
+        }),
+        viteReact(),
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
+    }
+})
+
+export default config
