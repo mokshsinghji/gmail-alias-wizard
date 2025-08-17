0340addc3571f44816664245e81d11cd3a4a4447
mokshsinghji <moksh@choudry.org>
2025-08-17 15:54:01 +0100

    changes for adding react frontend

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -2,14 +2,27 @@ package com.moksh
 
 import com.moksh.config.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.react
+import io.ktor.server.http.content.singlePageApplication
+import io.ktor.server.routing.routing
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+
+    routing {
+        singlePageApplication {
+            useResources = true
+            filesPath = "www"
+            defaultPage = "_shell.html"
+        }
+    }
+
     configureSecurity()
     configureSerialization()
     configureFrameworks()
     configureRouting()
+
 }
